705f9a6b1c96e1767e84546a2f1d140626670b79
Oliver Robson <oliver.robson.ecse@gmail.com>
2024-08-20 23:17:20 +1000

    Change context manager to use official json source again

--- src/components/compositions/data-retrieval.tsx
+++ src/components/compositions/data-retrieval.tsx
@@ -1,10 +1,15 @@
 import { Manifest } from "@/lib/manifest";
-import { PropsWithChildren, ReactNode, createContext, useEffect, useState } from "react";
-
+import {
+  PropsWithChildren,
+  ReactNode,
+  createContext,
+  useEffect,
+  useState,
+} from "react";
 
 interface DataContextType {
-    data: Manifest | null;
-    fetchData: () => Promise<Manifest>;
+  data: Manifest | null;
+  fetchData: () => Promise<Manifest>;
 }
 
 export const DataContext = createContext<DataContextType>({
@@ -13,11 +18,12 @@ export const DataContext = createContext<DataContextType>({
 });
 
 export function DataService({ children }: PropsWithChildren): ReactNode {
-
   const [data, setData] = useState<Manifest | null>(null);
 
   const handleFetchData: () => Promise<Manifest> = async () => {
-    const dt = await fetch("https://wb5lsudgvpt4f4j4pxomg5z3ui0dcgjt.lambda-url.ap-southeast-2.on.aws/").then((res) => res.json());
+    const dt = await fetch("https://micropython.org/pi/v2/index.json").then(
+      (res) => res.json()
+    );
     setData(dt);
     return dt;
   };
@@ -25,15 +31,13 @@ export function DataService({ children }: PropsWithChildren): ReactNode {
   const dataContext: DataContextType = {
     data,
     fetchData: handleFetchData,
-  }
+  };
 
   useEffect(() => {
     handleFetchData();
   }, []);
 
   return (
-    <DataContext.Provider value={dataContext}>
-      {children}
-    </DataContext.Provider>
+    <DataContext.Provider value={dataContext}>{children}</DataContext.Provider>
   );
 }
