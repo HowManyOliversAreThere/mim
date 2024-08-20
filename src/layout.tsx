7427a365bb0f49e62998c34f4ff6b6787f74e5ba
Oliver Robson <oliver.robson.ecse@gmail.com>
2024-08-20 23:20:09 +1000

    Fix site logo

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,20 +1,22 @@
-import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react';
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
-import { Outlet } from 'react-router-dom';
-import { Search } from './components/compositions/search';
-import { H2 } from './components/ui/typography';
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Outlet } from "react-router-dom";
+import { Search } from "./components/compositions/search";
+import { H2 } from "./components/ui/typography";
 
 interface NavigationItem {
   name: string;
   path: string;
 }
-const navigation: NavigationItem[] = [
-  { name: 'Packages', path: '/'},
-]
+const navigation: NavigationItem[] = [{ name: "Packages", path: "/" }];
 
 function classNames(...classes: (string | undefined | null | false)[]): string {
-    return classes.filter(Boolean).join(' ');
-  }
+  return classes.filter(Boolean).join(" ");
+}
 
 export default function Layout() {
   const currentPath = window.location.pathname;
@@ -30,30 +32,22 @@ export default function Layout() {
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div className="flex h-16 justify-between">
               <div className="flex">
-                <div className="flex flex-shrink-0 items-center">
-                <H2>
-          <code>
-            <a
-              href="https://docs.micropython.org/en/latest/reference/packages.html"
-              target='_blank'
-              className="underline text-blue-400"
-            >
-              mip
-            </a>
-          </code>
-        </H2>
+                <div className="flex flex-shrink-0 items-center mt-2">
+                  <H2>mim</H2>
                 </div>
                 <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
                   {navigation.map((item) => (
                     <a
                       key={item.name}
                       href={item.path}
-                      aria-current={checkIsCurrentPage(item)? 'page' : undefined}
+                      aria-current={
+                        checkIsCurrentPage(item) ? "page" : undefined
+                      }
                       className={classNames(
                         checkIsCurrentPage(item)
-                          ? 'border-slate-900 text-gray-900'
-                          : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
-                        'inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium',
+                          ? "border-slate-900 text-gray-900"
+                          : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
+                        "inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium"
                       )}
                     >
                       {item.name}
@@ -69,8 +63,14 @@ export default function Layout() {
                 <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2">
                   <span className="absolute -inset-0.5" />
                   <span className="sr-only">Open main menu</span>
-                  <Bars3Icon aria-hidden="true" className="block h-6 w-6 group-data-[open]:hidden" />
-                  <XMarkIcon aria-hidden="true" className="hidden h-6 w-6 group-data-[open]:block" />
+                  <Bars3Icon
+                    aria-hidden="true"
+                    className="block h-6 w-6 group-data-[open]:hidden"
+                  />
+                  <XMarkIcon
+                    aria-hidden="true"
+                    className="hidden h-6 w-6 group-data-[open]:block"
+                  />
                 </DisclosureButton>
               </div>
             </div>
@@ -83,12 +83,12 @@ export default function Layout() {
                   key={item.name}
                   as="a"
                   href={item.path}
-                  aria-current={checkIsCurrentPage(item) ? 'page' : undefined}
+                  aria-current={checkIsCurrentPage(item) ? "page" : undefined}
                   className={classNames(
                     checkIsCurrentPage(item)
-                      ? 'border-slate-500 bg-slate-50 text-slate-700'
-                      : 'border-transparent text-gray-600 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800',
-                    'block border-l-4 py-2 pl-3 pr-4 text-base font-medium',
+                      ? "border-slate-500 bg-slate-50 text-slate-700"
+                      : "border-transparent text-gray-600 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800",
+                    "block border-l-4 py-2 pl-3 pr-4 text-base font-medium"
                   )}
                 >
                   {item.name}
@@ -97,16 +97,16 @@ export default function Layout() {
             </div>
             <div className="border-t border-gray-200 pb-3 pt-4">
               <div className="flex items-center px-4">
-              <Search />
+                <Search />
               </div>
             </div>
           </DisclosurePanel>
         </Disclosure>
 
         <div className="p-10">
-                <Outlet />
+          <Outlet />
         </div>
       </div>
     </>
-  )
+  );
 }
